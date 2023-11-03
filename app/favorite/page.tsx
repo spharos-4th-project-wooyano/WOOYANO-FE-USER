1a2518e5d099994b7e09f4f279c6126a1d0a45e5
TheUNIONGraphix <jason@TheUNIONGraphixs-MacBook-Air.local>
2023-11-03 15:04:13 +0900

    [reboot] clear

--- app/favorite/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Favoritepage() {
-  return (
-    <div>Favoritepage</div>
-  )
-}
-
-export default Favoritepage
--- app/servicehistory/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ServiceHistorypage() {
-  return (
-    <div>ServiceHistorypage</div>
-  )
-}
-
-export default ServiceHistorypage
