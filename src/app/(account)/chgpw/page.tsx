9a908ddbf9de6f786c4874f9f93395315de5eec3
dow-dad <sojunyeong301@gmail.com>
2023-11-13 19:12:38 +0900

    Fix : Build Error
    빌드 에러 수정

--- src/app/(account)/chgpw/page.tsx
+++ src/app/(account)/chgpw/page.tsx
@@ -1,14 +1,10 @@
-'use client'
 import React from "react";
 import ChgPwStepper from "./chgPwStepper";
-import { SessionProvider } from "next-auth/react";
 
-export default function ChgPwPage(props: React.JSX.IntrinsicAttributes) {
+export default function ChgPwPage() {
   return (
-    <SessionProvider>
       <div className="container mb-24 lg:mb-32">
-        <ChgPwStepper {...props} />
+        <ChgPwStepper/>
       </div>
-    </SessionProvider>
   );
 }
