d3aa4042fd97227e679c26bda09eb0c40726c466
hibaek99 <tjdwjs9903@naver.com>
2023-11-17 09:49:52 +0900

    fix: 업체 상세리스트 리뷰 탭 수정
    업체 상세리스트 리뷰 탭 및 화면 수정

--- src/components/CommentListing.tsx
+++ src/components/CommentListing.tsx
@@ -1,13 +1,25 @@
 import { StarIcon } from "@heroicons/react/24/solid";
 import React, { FC } from "react";
 import Avatar from "@/shared/Avatar";
+import ImgSwiper from "./function/imgSwiper";
+import { Button } from "@nextui-org/button";
+import ButtonPrimary from "@/shared/ButtonPrimary";
+import Image from "next/image";
+import room from "@/images/test/room.png"
+import room2 from "@/images/test/room2.png"
+import {Card, CardBody} from "@nextui-org/react";
 
 interface CommentListingDataType {
   name: string;
   avatar?: string;
   date: string;
-  comment: string;
+  serviceDate: string;
+  desc: string;
   starPoint: number;
+  res: string;
+  worker: string;
+  service: string;
+  comment: string[];
 }
 
 export interface CommentListingProps {
@@ -16,13 +28,23 @@ export interface CommentListingProps {
   hasListingTitle?: boolean;
 }
 
-const DEMO_DATA: CommentListingDataType = {
-  name: "Cody Fisher",
-  date: "May 20, 2021",
-  comment:
-    "There’s no stopping the tech giant. Apple now opens its 100th store in China.There’s no stopping the tech giant.",
+const DEMO_DATA: CommentListingDataType =
+  {
+  name: "이훙훙",
+  date: "2023.10.20",
+  serviceDate: "2023.10.15",
+  desc: "꼼꼼하게 잘 해주셔서 너무 좋았습니당:) !!!",
   starPoint: 5,
-};
+  res: "다음에도 이용할게요:)",
+  worker: "김하늘",
+  service: "가사도우미 - 원룸",
+  comment: [
+    "저희 서비스를 이용해주셔서 감사합니다^^!", 
+    "다음 번에도 이용해주세요-!!"
+  ],
+  }
+
+
 
 const CommentListing: FC<CommentListingProps> = ({
   className = "",
@@ -31,46 +53,91 @@ const CommentListing: FC<CommentListingProps> = ({
 }) => {
   return (
     <div
-      className={`nc-CommentListing flex space-x-4 ${className}`}
+      className={`nc-CommentListing flex justify-center space-x-4 ${className}`}
       data-nc-id="CommentListing"
     >
-      <div className="pt-0.5">
+      <div className="p-0.5">
         <Avatar
-          sizeClass="h-10 w-10 text-lg"
-          radius="rounded-full"
+          // sizeClass="h-10 w-10 text-lg"
+          // radius="rounded-full"
           userName={data.name}
-          imgUrl={data.avatar}
+          // imgUrl={data.avatar}
         />
       </div>
       <div className="flex-grow">
         <div className="flex justify-between space-x-3">
           <div className="flex flex-col">
             <div className="text-sm font-semibold">
+              {/* 작성자명 */}
               <span>{data.name}</span>
-              {hasListingTitle && (
+              {/* {hasListingTitle && (
                 <>
                   <span className="text-neutral-500 dark:text-neutral-400 font-normal">
                     {` review in `}
                   </span>
                   <a href="/">The Lounge & Bar</a>
                 </>
-              )}
+              )} */}
             </div>
+            {/* 작성일자 */}
             <span className="text-sm text-neutral-500 dark:text-neutral-400 mt-0.5">
               {data.date}
             </span>
           </div>
-          <div className="flex text-yellow-500">
+          {/* 평가 */}
+          <div className="text-xl font-semibold text-secondary-6000">
+            {data.res}
+          </div>
+          {/* <div className="flex text-yellow-500">
             <StarIcon className="w-4 h-4" />
             <StarIcon className="w-4 h-4" />
             <StarIcon className="w-4 h-4" />
             <StarIcon className="w-4 h-4" />
             <StarIcon className="w-4 h-4" />
+          </div> */}
+        </div>
+        <div className="flex justify-center items-center my-4">
+          <div className="w-[500px]">
+            <ImgSwiper/>
           </div>
         </div>
-        <span className="block mt-3 text-neutral-6000 dark:text-neutral-300">
-          {data.comment}
-        </span>
+
+        {/* 작업자 및 서비스 */}
+        <div className="flex justify-end text-xs text-gray-500 mt-5 mb-2 gap-3">
+          <div className="border border-dotted border-slate-400 rounded-2xl leading-8 px-3" style={{ borderWidth: '3px' }}>
+            {data.worker} 기사님
+          </div>
+          <div className="border border-dotted border-slate-400 rounded-2xl leading-8 px-3 " style={{ borderWidth: '3px' }}>
+            {data.service}
+          </div>
+        </div>
+
+        <div className="border border-white rounded-2xl relative px-4 mt-5 bg-slate-100">
+          {/* 리뷰 내용 */}
+          <span className="text-slate-100 text-[36px] absolute top-[-19px] left-[3px]">
+            ▲
+          </span>
+          <span className="block text-black dark:text-neutral-300 leading-10">
+            {data.desc}
+          </span>
+        </div>
+        
+
+        {/* 리뷰 답글 */}
+        <div className="bg-blue-100 rounded-2xl relative leading-10 px-5 mt-8 break-all">
+          <span className="text-blue-100 text-[36px] absolute bottom-[-29px] right-[3px]">
+            ▼
+          </span>
+          <span>
+            {DEMO_DATA.comment.map((sentence, index) => (
+            <React.Fragment key = {index}>
+              {sentence}
+                <br />
+            </React.Fragment>
+            ))}
+          </span>
+        </div>
+
       </div>
     </div>
   );
--- src/components/detail/worker/worker.tsx
+++ src/components/detail/worker/worker.tsx
@@ -9,29 +9,33 @@ function Worker({setOnClickData,data}:{setOnClickData:React.Dispatch<SetStateAct
       workername:data.name,
       service:data.service,
       desc:data.desc,
-    })}}>
+    })}} className='py-5'>
       <div className="flex items-center">
         <Avatar
-          hasChecked
-          hasCheckedClass="w-4 h-4 -top-0.5 right-0.5"
+          // hasChecked
+          // hasCheckedClass="w-4 h-4 -top-0.5 right-0.5"
           sizeClass="h-14 w-14"
-          radius="rounded-full"
+          radius="rounded-xl"
         />
         <div className='ml-3'>
-          <a className="block text-xl font-medium" href="##">
+          <a className="flex block text-xl text-black font-medium" href="##">
             {data.name}
+            <p className='flex items-center text-sm text-gray-500'>
+              기사
+            </p>
           </a>
           <div className="mt-1.5 flex items-center text-sm text-neutral-500 dark:text-neutral-400 gap-2">
           <FaStar className="fill-yellow-400" />
           <p>{`(${data.star})`}</p>
           </div>
         </div>
+        {/* desc */}
+        <span className="block text-neutral-6000 dark:text-neutral-300 mt-5 ml-10">
+          {data.sub_desc}
+        </span>
       </div>
 
-      {/* desc */}
-      <span className="block text-neutral-6000 dark:text-neutral-300 mt-5">
-        {data.sub_desc}
-      </span>
+      
     </div>
   )
 }
